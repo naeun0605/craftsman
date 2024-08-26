3f68fce3ff629f4c0c6530bf18829de65610884a
naeun0605 <ne9506@gmail.com>
2024-08-26 21:37:49 +0900

    0826

---
+++ craftsman_web_design/a3/script/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    // 모달
+    $("#tabs>ul li").click(function () {
+        var i = $(this).index();
+        $("#tabs>div").css({
+            display: "none"
+        });
+        $("#tabs>div").eq(i).css({
+            display: "block"
+        });
+    });
+   
+    // 팝업
+    $("#tabs>div li").eq(0).click(function(){
+        $("#modal").show();
+    });
+    $(".popup button").click(function(){
+        $("#modal").hide();
+    });
+});
+
+
+
+
+
+
+
+
